674ae1576d41a59be96372ad72da66c4b50621e4
Peter Lindberg <peter@fleecelabs.se>
2016-09-07 00:09:20 +0200

    Use proxy in main module to detach setting/getting

--- index.js
+++ index.js
@@ -3,8 +3,19 @@
 
 const nullLogger = require('./lib/null-logger');
 
-exports.log = nullLogger;
+let logger = nullLogger;
+
+const proxy = Object.freeze({
+  fatal() { logger.fatal.apply(logger, arguments); },
+  error() { logger.error.apply(logger, arguments); },
+  warn() { logger.warn.apply(logger, arguments); },
+  info() { logger.info.apply(logger, arguments); },
+  debug() { logger.debug.apply(logger, arguments); },
+  trace() { logger.trace.apply(logger, arguments); }
+});
+
+exports.log = proxy;
 
 exports.setLogger = function (newLogger) {
-  exports.log = newLogger;
+  logger = newLogger;
 };
